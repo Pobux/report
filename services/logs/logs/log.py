e710f5039cf3d6c68f4855e24bbce94ab7853b1c
Pobux <contact@me.com>
2018-10-27 10:32:05 -0400

    added test

---
+++ services/logs/logs/log.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+# Creation Date : 2018-10-27
+# Created by : Antoine LeBel
+class Log():
+    def __init__(self):
+        pass
+
+    def say_hello(self):
+        return "hello"
